1121015999af8f6b1259c7e8988002d72da4b6f3
HassanAlti <hassanalti@outlook.com>
2024-09-03 15:58:37 +0300

    implement mongo

---
+++ src/auth.config.ts
@@ -0,0 +1,13 @@
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import type { NextAuthConfig } from "next-auth";
+
+// Notice this is only an object, not a full Auth.js instance
+export default {
+  providers: [GitHub, Google],
+  callbacks: {
+    authorized: async ({ auth }) => {
+      return !!auth;
+    },
+  },
+} satisfies NextAuthConfig;
--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,10 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+import authConfig from "./auth.config";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import client from "@/lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub, Google],
-  callbacks: {
-    authorized: async ({ auth }) => {
-      return !!auth;
-    },
-  },
+  adapter: MongoDBAdapter(client),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
