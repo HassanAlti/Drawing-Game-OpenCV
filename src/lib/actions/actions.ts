67387fc4585bc0d114740e9bb5a7d6118e0ec86c
HassanAlti <hassanalti@outlook.com>
2024-09-13 18:02:57 +0300

    implement user based server actions

--- src/lib/actions/actions.ts
+++ src/lib/actions/actions.ts
@@ -1,45 +1,71 @@
 "use server";
 
 import client from "@/lib/db";
+import { getSession } from "next-auth/react";
 
 export const saveScore = async (score: number, currentImage: string) => {
   try {
+    const session = await getSession();
+    if (!session || !session.user) {
+      throw new Error("User not authenticated");
+    }
+
+    const userId = session.user.id;
+
     await client.db("ai-drawing-game").collection("scores").insertOne({
+      userId,
       score,
       image: currentImage,
       createdAt: new Date(),
     });
   } catch (error) {
     console.error("Error in saveScore:", error);
+    throw error;
   }
 };
 
 export const highScoreForImage = async (image: string) => {
   try {
+    const session = await getSession();
+    if (!session || !session.user) {
+      throw new Error("User not authenticated");
+    }
+
+    const userId = session.user.id;
+
     const result = await client
       .db("ai-drawing-game")
       .collection("scores")
-      .find({ image })
+      .find({ userId, image })
       .sort({ score: -1 })
       .limit(1)
       .toArray();
     return result.length > 0 ? { score: result[0].score } : { score: null };
   } catch (error) {
     console.error("Error in highScoreForImage:", error);
+    throw error;
   }
 };
 
 export const allTimeHighScore = async () => {
   try {
+    const session = await getSession();
+    if (!session || !session.user) {
+      throw new Error("User not authenticated");
+    }
+
+    const userId = session.user.id;
+
     const result = await client
       .db("ai-drawing-game")
       .collection("scores")
-      .find()
+      .find({ userId })
       .sort({ score: -1 })
       .limit(1)
       .toArray();
     return result.length > 0 ? { score: result[0].score } : { score: null };
   } catch (error) {
     console.error("Error in allTimeHighScore:", error);
+    throw error;
   }
 };
